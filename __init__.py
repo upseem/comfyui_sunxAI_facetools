4d9610433dc28b33115a1b82547221b78014d364
陈亮 <cody@51cashbox.com>
2025-08-05 10:26:05 +0800

    feat: initial scaffold for comfyui_sunxAI_facetools

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for comfyui_sunxAI_facetools."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Sunx.ai"""
+__email__ = "sunx.ai@hotmail.com"
+__version__ = "0.0.1"
+
+from .src.comfyui_sunxAI_facetools.nodes import NODE_CLASS_MAPPINGS
+from .src.comfyui_sunxAI_facetools.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
